82f10d4bdc8c1950b0fd8d49df7bd05ab65aad30
natalie korn <natalie.j.korn@gmail.com>
2021-05-06 16:24:18 -0700

    poc radial chart

--- plot_utils.py
+++ plot_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
@@ -7,7 +8,7 @@ def donut_by_species(df):
 
 def bar_price(df):
     data = [go.Bar(
-        x=df['Name'],  # NOC stands for National Olympic Committee
+        x=df['Name'],
         y=df['Price']
     )]
     layout = go.Layout(
@@ -15,3 +16,48 @@ def bar_price(df):
     )
     fig = go.Figure(data=data, layout=layout)
     pyo.plot(fig, filename='fish_prices.html')
+
+def bar_abundance(df):
+
+    data = [go.Bar(
+        x=df['Name'],
+        y=df['Price']
+    )]
+    layout = go.Layout(
+        title='Fish Prices'
+    )
+    fig = go.Figure(data=data, layout=layout)
+    pyo.plot(fig, filename='fish_prices.html')
+
+def radial_year(df, months):
+
+
+    trace1 = go.Scatterpolar(
+        #r = [np.nan,4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,np.nan,np.nan],
+        r = df[df['Name'] == "Catfish"][months].values.flatten().tolist(),
+        theta  = months,
+        mode='lines',
+        line=dict(
+            shape='spline',
+            smoothing=1,
+            width=10
+        ),
+        name='Catfish'
+    )
+
+    trace2 = go.Scatterpolar(
+        #r = [np.nan,4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,0.4,np.nan,np.nan],
+        r = df[df['Name'] == "Gar"][months].values.flatten().tolist(),
+        theta  = months,
+        mode='lines',
+        line=dict(
+            shape='spline',
+            smoothing=1,
+            width=10
+        ),
+        name='Gar'
+    )
+
+    data = [trace1, trace2]
+    fig = go.Figure(data=data)
+    pyo.plot(fig, filename='radial_test.html')
